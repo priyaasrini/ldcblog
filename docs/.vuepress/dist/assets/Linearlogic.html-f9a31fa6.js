be60977f4d33546f63f1aa11446cc2b5389868d8
pvarshinee <priyaa.mirra@gmail.com>
2025-09-19 16:35:26 +0300

    www.lineardistributivity.org to lineardistributivity.org

--- docs/.vuepress/dist/assets/Linearlogic.html-f9a31fa6.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-e2d38cdc","path":"/chapter1/Linearlogic.html","title":"E1. An introduction to linear logic","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1689296013000,"contributors":[{"name":"pvarshinee","email":"priyaa.mirra@gmail.com","commits":5}]},"filePathRelative":"chapter1/Linearlogic.md"}');export{e as data};
--- docs/deploy.sh
+++ docs/deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'www.lineardistributivity.org' > CNAME
+echo 'lineardistributivity.org' > CNAME
 
 git init
 git add -A
